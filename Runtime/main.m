a20a071465fb5a812c385432413201a302a966c6
cinderella <50831606+tangaoyang@users.noreply.github.com>
2020-07-17 17:50:19 +0800

    7.17下午

---
+++ Runtime/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Runtime
+//
+//  Created by cinderella on 2020/7/17.
+//  Copyright © 2020 cinderella. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
